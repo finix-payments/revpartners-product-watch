46b8e11797525afc491940f04fd08cdee80a2ada
Tom Van Buskirk <tom.vanbuskirk@finix.com>
2025-08-19 14:21:25 -0700

    DEVOPS-4477: exclude git and terraform files from archive
    The zip file used for the lambda function was huge because it
    included uneccessary terraform and git files
    
    This also updates the script to use a newer version of the aws-sdk
    library

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,6 +1,7 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/.." # repo root (one level up from infra/)
+  excludes    = ["terraform", ".git"]
   output_path = "${path.module}/lambda.zip"
 }
 
